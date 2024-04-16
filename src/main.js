d553ebc6b54a88903b750d3ef0db4ebba2c11ef4
Maksim <45427901+flydecisive@users.noreply.github.com>
2024-04-16 11:11:43 +0300

    add pwa

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 // import { store } from "./store/store";
 import App from "./App.vue";
 import "./style.scss";
+import './registerServiceWorker'
 
 createApp(App).mount("#app");
